a0bf07ede41e1ace1fb06ac534e3c97ca2de0849
Garima27sharma <garimasharma3281@gmail.com>
2021-07-23 21:00:00 +0530

    add files

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+setx POSTGRES_USERNAME udagramdb2
+setx POSTGRES_PASSWORD udagramdb2
+setx POSTGRES_DB postgres
+setx AWS_BUCKET test-661939815197-bucket
+setx POSTGRES_HOST udagramdb2.cwfzwmp9ek62.us-east-2.rds.amazonaws.com
+setx AWS_REGION us-east-1
+setx AWS_PROFILE default
+setx JWT_SECRET hello
+setx URL http://localhost:8100
