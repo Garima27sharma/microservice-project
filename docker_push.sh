aa44adadd239583ac169b2be529ddeecff280f74
Garima27sharma <57446666+Garima27sharma@users.noreply.github.com>
2021-07-30 18:24:06 +0530

    Delete docker_push.sh

--- docker_push.sh
+++
@@ -1,8 +0,0 @@
-#echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-# echo $env:DOCKER_PASSWORD | docker login -u $env:DOCKER_USERNAME --password-stdin  ## for windows
-# docker login -u $env:DOCKER_USERNAME -p $env:DOCKER_PASSWORD
-
-#docker-compose -f docker-compose-build.yaml push
-
-
-
